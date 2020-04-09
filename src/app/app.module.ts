88eab360845d07bf07e97e2429b98f3276f5fcdf
TulebaevTemirlan <47266010+TulebaevTemirlan@users.noreply.github.com>
2020-04-10 00:31:47 +0600

    Ukadan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SharedComponent } from './shared/shared.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { CategoryComponent } from './shared/category/category.component';
+import { NewsDetailComponent } from './news-detail/news-detail.component';
+import { NewsItemComponent } from './news-item/news-item.component';
+import { NewsListComponent } from './news-list/news-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavbarComponent,
+    CategoryComponent,
+    NewsDetailComponent,
+    NewsItemComponent,
+    NewsListComponent
   ],
   imports: [
     BrowserModule
